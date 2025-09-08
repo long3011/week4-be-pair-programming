1dec2a3d78d758cf4ce97cafffd5ac7b8df9b979
aashish0770 <ssuzzan81@gmail.com>
2025-09-08 14:08:02 +0300

    step 4, 5

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const tourRouter = require("./routes/tourRouter.js");
 const userRouter = require("./routes/userRouter.js");
+const morgan = require("morgan");
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(morgan("tiny"));
 
 // ROUTES
-app.use("/tours", tourRouter);
-app.use("/users", userRouter);
+app.use("/api/tours", tourRouter);
+app.use("/api/users", userRouter);
 
 const port = 4000;
 // Start the server
